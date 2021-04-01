b6ed5e2a05adae4834f4e54a647a974949dd6472
Pravoslav <pravoslav.zilka@gmail.com>
2021-04-01 17:16:48 +0200

    register system added

--- __init__.py
+++ __init__.py
@@ -46,12 +46,43 @@ def login_page2():
     return redirect(url_for('home_page'))
 
 
-@app.route("/sign_out")
+@app.route("/sign_out/")
 @login_required
 def logout():
     logout_user()
     flash("We looking forward to see you again !","success")
     return redirect(url_for("home_page"))
+
+
+@app.route('/sign_up/')
+def register_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    return render_template("register_page.html")
+
+
+@app.route('/sign_up/',methods=["POST"])
+def register_page2():
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+    user1 = User.query.filter(User.email == email).first()
+    user2 = User.query.filter(User.name == username).first()
+
+    if user1:
+        flash("User with this email address already exist.","danger")
+        return render_template("register_page.html",username=username)
+    elif user2:
+        flash("User with this username already exist.","danger")
+        return render_template("register_page.html", email=email)
+    else:
+        u = User(username,email)
+        u.set_password(password)
+        db_session.add(u)
+        db_session.commit()
+        flash("Welcome to our site !","success")
+        login_user(u)
+        return redirect(url_for("home_page"))
 
 
 @login_manager.user_loader
